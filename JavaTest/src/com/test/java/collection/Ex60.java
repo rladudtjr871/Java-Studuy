96abf4eec9e64239c24c93f4e12b0abf9d0f7860
rladudtjr871 <rladudtjr871@naver.com>
2022-04-01 18:42:45 +0900

    ArrayList + HashMap

---
+++ JavaTest/src/com/test/java/collection/Ex60.java
@@ -0,0 +1,155 @@
+package com.test.java.collection;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Ex60 {
+
+	public static void main(String[] args) {
+
+		//ArrayList + HashMap
+		
+		//학생 성적 처리
+		
+		//학생(국어, 영어, 수학) x N명
+		
+		//국어, 영어, 수학 > 한 곳에 저장할 자료형
+		//1. 배열(ArrayList)
+		//2. HashMap
+		//3. 클래스
+		
+		
+		//Case 1. Array(ArrayList)
+		//- 루프 사용
+		//- 비용 낮음
+		//- 가독성 문제 > 과목을 식별 > 방번호
+		int[] score = new int[3];
+		
+		score[0] = 100; //국어
+		score[1] = 90;  //영어
+		score[2] = 80;  //수학
+		
+		
+		//Case 2. HashMap
+		HashMap<String, Integer> scoreMap = new HashMap<String, Integer> ();
+		//-가독성 높음 > 과목을 식벌 > 이름(Key)
+		//- 루프 사용 불가능
+		//- 비용 낮음 > 틀을 안 만든다. > 장점이자 단점!!! > Key값이 틀리면 큰일!!
+		//- 객체를 1개만 만들 때는 Class보다 유리
+		
+		scoreMap.put("국어", 100);
+		scoreMap.put("영어", 90);
+		scoreMap.put("수학", 80);
+		
+		
+		HashMap<String, Integer> scoreMap2 = new HashMap<String, Integer> ();
+		
+		scoreMap2.put("극이", 100);
+		scoreMap2.put("영어", 90);
+		scoreMap2.put("수학", 80);
+		
+		
+		
+		//Case 3. Class
+		Score scoreObject = new Score();
+		
+		scoreObject.set국어(100);
+		scoreObject.set영어(90);
+		scoreObject.set수학(80);
+		
+		
+		Score scoreObject2 = new Score();
+		
+		scoreObject2.set국어(100);
+		scoreObject2.set영어(90);
+		scoreObject2.set수학(80);
+		
+		
+		//HashMap & Class
+		//- 내부 요소를 이름으로 구분(***)
+		//- 생산 비용이 다르다
+		
+		
+		//학생 x 3명
+		//HashMap<String, Integer>[] list = new HashMap<String, Integer>[3];
+		
+		//2차원 배열
+		ArrayList<HashMap<String, Integer>> list = new ArrayList<HashMap<String,Integer>>();
+		
+		HashMap<String, Integer> s1 = new HashMap<String,Integer>();
+		s1.put("국어", 100);
+		s1.put("영어", 90);
+		s1.put("수학", 80);
+		
+		list.add(s1); //2차원 배열.add(1차원 배열)
+		
+		
+		HashMap<String, Integer> s2 = new HashMap<String,Integer>();
+		s2.put("국어", 95);
+		s2.put("영어", 85);
+		s2.put("수학", 75);
+		
+		list.add(s2);
+		
+		
+		HashMap<String, Integer> s3 = new HashMap<String,Integer>();
+		s3.put("국어", 93);
+		s3.put("영어", 83);
+		s3.put("수학", 73);
+
+		list.add(s3);
+		
+		
+		//[
+		//  {국어=100, 수학=80, 영어=90},
+		//  {국어=95, 수학=75, 영어=85},
+		//  {국어=93, 수학=73, 영어=83}
+		//]
+
+		System.out.println(list);
+		
+		
+		
+		
+
+	}//main
+
+}//Ex60
+
+
+//Case 3. Class
+//- 가독성 높음, HashMap 유사
+//- 멤버에 루프 적용 불가능, HashMap 유사
+//- 비용 높음
+//- 객체를 2개 이상 만들 땐 HashMap보다 유리(오타를 줄일 수 있음)
+
+class Score {
+	
+	private int 국어;
+	private int 영어;
+	private int 수학;
+	
+	public int get국어() {
+		return 국어;
+	}
+	public void set국어(int 국어) {
+		this.국어 = 국어;
+	}
+	public int get영어() {
+		return 영어;
+	}
+	public void set영어(int 영어) {
+		this.영어 = 영어;
+	}
+	public int get수학() {
+		return 수학;
+	}
+	public void set수학(int 수학) {
+		this.수학 = 수학;
+	}
+	
+	
+	
+	
+	
+}
