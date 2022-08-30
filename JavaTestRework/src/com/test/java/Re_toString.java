6580537bcff3268beba73b469be36958289aa1d4
rladudtjr871 <rladudtjr871@naver.com>
2022-08-30 22:07:14 +0900

    "0830"

---
+++ JavaTestRework/src/com/test/java/Re_toString.java
@@ -0,0 +1,48 @@
+package com.test.java;
+
+public class Re_toString {
+
+	public static void main(String[] args) {
+
+/*
+ 	
+ 		[SUMMARY] Object 클래스의 toString() 오버라이드 
+ 	
+ 		
+ 		모든 주소값을 저장하는 변수는 int형(4byte)
+		String변수도 주소값을 저장하는 상자 자체는 int형이다. > 메모리 주소 > int 사용
+ 
+ 
+ 		객체의 출력 > 객체의 toString() 반환값을 출력
+ 		  
+ 		 		  
+ 		t1과 now의 toString 차이가 나는 이유
+		now.toString() : String - Date(오버라이드)
+		Date클래스에서 toString을 재정의 했다는 뜻
+ 		 
+ 		 
+ 		 
+ 		Object.toString()
+		- 자식 클래스에서 그대로 사용하지 않는다. > 쓸모 없음..
+		- 반드시 메소드 오버라이드(재정의)를 해서 사용한다.
+		- 모든 객체는 toString() 소유
+		- (toString)어떤 코드를 재정의? > 자신이 소유한 데이터(멤버 변수)의 값을 문자열로 돌려주는 코드
+									> 덤프(dump)
+		- toString은 규칙 역할 > 이런걸 만들어라
+ 		 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
+		
+		
+		
+
+	}
+
+}
